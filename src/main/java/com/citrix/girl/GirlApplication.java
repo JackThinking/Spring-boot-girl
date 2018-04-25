f5bd1a79492495bf1a7600c5a0ba6caf5eb84c12
Citrix <sijieqian@foxmail.com>
2018-04-25 21:35:48 +0800

    425学习

---
+++ src/main/java/com/citrix/girl/GirlApplication.java
@@ -0,0 +1,14 @@
+package com.citrix.girl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Component;
+
+
+@SpringBootApplication
+public class GirlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GirlApplication.class, args);
+	}
+}
